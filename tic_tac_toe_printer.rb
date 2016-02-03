1e17ae3838e6b2f4dcbdeceada8832f74f528be0
Evan Watterud <cod.videos@hotmail.com>
2016-02-03 18:29:59 -0500

    Final commit

--- tic_tac_toe_printer.rb
+++ tic_tac_toe_printer.rb
@@ -1,17 +1,16 @@
 def print_grid(board)
-  line_count = 0
-  char_count = 0
-
-  board.each do |line|
-    line.each do |char|
+  board.each_with_index do |val, index|
+    val.each_with_index do |char, index|
       if char.nil?
         print "   "
       else
         print " #{char} "
       end
+      print "|" unless index == 2
     end
-    puts "\n---------\n"
+    puts "\n-----------\n" unless index == 2
   end
+  puts ""
 end
 
 board_a = [
@@ -20,4 +19,11 @@ board_a = [
   ['x', 'o', nil]
 ]
 
+board_b = [
+  [nil, 'o', 'x'],
+  ['x', 'o', nil],
+  ['x', 'o', nil]
+]
+
 print_grid(board_a)
+print_grid(board_b)
